dd4c3c7058d11cd454e4b489c2dcca45ce463fd4
NycolasFreitasL <nycolasfretasetec@gmail.com>
2026-08-11 13:49:32 -0300

    atualizando login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,40 @@
+"use client";
 import { useState } from "react";
 
 const page = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault(); console.log(email, password); };
+
+    const [error,setError] = useState("");
+
+    if (email === "" ) {
+        () => setError("O campo de e-mail é obrigatório.");
+    }
+    else if (password === "" ) {
+        () => setError("O campo de senha é obrigatório.");
+    }else {
+        () => setError("");
+    }
+        
+    
+    
   return (
     <main>
         <>
             <h1>Login</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>E-mail</label>
-                <input type="email" placeholder="Digite seu e-mail" onChange={(e)=> setEmail(e.target.value)}/>
+                <input type="email" placeholder="Digite seu e-mail" value={email} onChange={(e)=> setEmail(e.target.value)}/>
 
                 <label>Senha</label>
-                <input type="password" placeholder="Digite sua senha"  onChange={(e) => setPassword(e.target.value)}/>
+                <input type="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
                 <button type="submit">Entrar</button>
             </form>
         </>
+        <p>{error} </p>
     </main>
   )
 }
