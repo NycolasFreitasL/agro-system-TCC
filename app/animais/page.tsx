26eba5b1e5c719558eefe4c124f2e6f300f761b5
NycolasFreitasL <nycolasfretasetec@gmail.com>
2026-08-25 11:43:25 -0300

    criando sidebar component

---
+++ app/animais/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Animais</h1>
+      <p>Gerenciamento dos animais da produção.</p>
+    </div>
+  );
+};
---
+++ app/plantio/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Estoque</h1>
+      <p>Gerenciamento do estoque.</p>
+    </div>
+  );
+};
